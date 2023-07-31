24ea2979b24350180580754758c3549b925a46f7
teng10 <adateng07@gmail.com>
2023-07-30 21:26:57 -0400

    style: minor fixes.

--- tn_generative/data_utils.py
+++ tn_generative/data_utils.py
@@ -5,16 +5,17 @@ import numpy as np
 def split_complex_ds(ds):
   for var in ds.data_vars:
     if np.iscomplexobj(ds[var].values):
-      ds[var + "_real"] = ds[var].real
-      ds[var + "_imag"] = ds[var].imag
+      ds[var + '_real'] = ds[var].real
+      ds[var + '_imag'] = ds[var].imag
       ds = ds.drop_vars(var)
   return ds
 
+
 def combine_complex_ds(ds):
   for var in ds.data_vars:
-    if "_real" in var:
+    if var.endswith('real'):
       var_real = var
-      var_imag = var[:-5] + "_imag"
+      var_imag = var[:-len('_real')] + '_imag'
       ds[var[:-5]] = ds[var_real] + 1.j * ds[var_imag]
       ds = ds.drop_vars([var_real, var_imag])
   return ds
