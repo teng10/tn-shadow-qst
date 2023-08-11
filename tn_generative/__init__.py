c7f924015d212390182ea3b7339bff9144975207
teng10 <adateng07@gmail.com>
2023-08-11 16:21:49 -0400

    chore: changed init file and edited requirements.

--- tn_generative/__init__.py
+++ tn_generative/__init__.py
@@ -1,11 +1,13 @@
 """tn_generative package."""
 from tn_generative import data_generation
+from tn_generative import data_utils
+from tn_generative import func_utils
 from tn_generative import lattices
 from tn_generative import mps_sampling
 from tn_generative import mps_utils
 from tn_generative import node_collections
-from tn_generative import func_utils
 from tn_generative import plotting_utils
 from tn_generative import physical_systems
+from tn_generative import regularizers
 from tn_generative import train_utils
 from tn_generative import types
