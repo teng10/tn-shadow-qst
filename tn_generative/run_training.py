35d485c7043d83aa6d80455363a19c516f3e4495
teng10 <adateng07@gmail.com>
2023-08-14 08:11:50 -0400

    dev: writing training configs using surface code example; enabling sweep training for multiple datasets; more work needs to be done to make the config cleaner.

--- tn_generative/run_training.py
+++ tn_generative/run_training.py
@@ -4,6 +4,8 @@ from absl import flags
 from datetime import datetime
 import os
 import pickle
+import glob
+import re
 
 from jax import config as jax_config
 from ml_collections import config_flags
@@ -27,28 +29,50 @@ DTYPES_REGISTRY = types.DTYPES_REGISTRY
 TASK_REGISTRY = data_generation.TASK_REGISTRY
 regularization = regularizers.REGULARIZER_REGISTRY
 
-
-def run_full_batch_experiment(exp_config):
+      
+def run_full_batch_experiment(config):
   current_date = datetime.now().strftime('%m%d')
   jax_config.update('jax_enable_x64', True)
   qtn.contraction.set_contract_backend('jax')
-  train_config = exp_config.training
-  model_config = exp_config.model
-  ds = xr.open_dataset(exp_config.data.path)
+  if config.sweep_name in config.sweep_fn_registry:
+    sweep_params = config.sweep_fn_registry[config.sweep_name]
+    config.update_from_flattened_dict(sweep_params[config.task_id])
+  elif config.sweep_name is None:
+    pass
+  else:
+    raise ValueError(f'Invalid sweep name {config.sweep_name}')
+  if config.data.filename_fn is not None:
+    config.data.filename = config.data.filename_fn(config.data.dir)
+    print(f'Using filename {config.data.filename}')
+    # TODO(YT): temporary solution to allow pickle. Find a better way!
+    config.unlock()
+    del config.data.filename_fn
+    del config.sweep_fn_registry
+    config.lock()
+  train_config = config.training
+  model_config = config.model    
+  datapath = os.path.join(config.data.dir, config.data.filename)
+  ds = xr.open_dataset(datapath)
   ds = data_utils.combine_complex_ds(ds)  # combine complex data into one ds.
-  train_ds = ds.isel(sample=slice(0, exp_config.data.num_training_samples))
-  #TODO(YT): better name for system?
-  system = TASK_REGISTRY[exp_config.task_name](**exp_config.task_kwargs)
+  train_ds = ds.isel(sample=slice(0, config.data.num_training_samples))
+  #TODO(YT): better way to reload attrs from dataset.
+  ds_attrs = ds.attrs.copy()
+  ds_attrs.pop('name')
+  physical_system = TASK_REGISTRY[ds.name](**ds_attrs)
   reg_fn = regularization[train_config.reg_name]
-  reg_fn = reg_fn(system=system, train_ds=train_ds, **train_config.reg_kwargs)
-  qugen.rand.seed_rand(model_config.init_seed)
+  reg_fn = reg_fn(
+      system=physical_system, train_ds=train_ds, 
+      **train_config.reg_kwargs
+  )
+  random_seed = model_config.init_seed + train_config.iterations
+  qugen.rand.seed_rand(random_seed)
   model_mps = qtn.MPS_rand_state(
       train_ds.sizes['site'], model_config.bond_dim, dtype=model_config.dtype)
   train_df, eval_df, final_mps = train_utils.run_full_batch_training(
       model_mps, train_ds, train_config, reg_fn)
 
   # massaging configs to store all experiment parameters.
-  config_df = pd.json_normalize(exp_config.to_dict(), sep='_')
+  config_df = pd.json_normalize(config.to_dict(), sep='_')
   complete_eval_df = pd.merge(
       eval_df, config_df, left_index=True, right_index=True, how='outer')
   tiled_config_df = pd.DataFrame(
@@ -57,12 +81,16 @@ def run_full_batch_experiment(exp_config):
   complete_train_df = pd.merge(
       train_df, tiled_config_df,
       left_index=True, right_index=True, how='outer')
-  if exp_config.output.save_results:
-    if not os.path.exists(exp_config.output.experiment_dir):
-      os.makedirs(exp_config.output.experiment_dir)
-    save_path = exp_config.output.results_save_path.replace(
-        '%date', current_date
+  if config.results.save_results:
+    results_dir = config.results.experiment_dir.replace(
+        '%CURRENT_DATE', current_date
     )
+    if not os.path.exists(results_dir):
+      os.makedirs(results_dir)
+    results_filename = config.results.filename.replace(
+        '%JOB_ID', str(config.job_id)
+    )
+    save_path = os.path.join(results_dir, results_filename)
     complete_train_df.to_pickle(save_path + '_train.pkl')
     complete_eval_df.to_pickle(save_path + '_eval.pkl')
     pickle.dump(final_mps, open(save_path + '_mps.pkl', 'wb'))
--- tn_generative/train_configs/surface_code_training_config.py
+++ tn_generative/train_configs/surface_code_training_config.py
@@ -1,12 +1,135 @@
 """config file for data generation"""
 import os
+import glob
+import re
 
 from ml_collections import config_dict
 
-home = os.path.expanduser('~')
+HOME = os.path.expanduser('~')
+DEFAULT_TASK_NAME = 'surface_code'
+
+
+def get_find_file_fn(sampler, system_size, onsite_z_field):
+  """Helper function for constructing filename."""
+  def get_filename_fn(data_dir):
+    regex = '_'.join([
+        DEFAULT_TASK_NAME, sampler, f'{system_size=}', 'd=(\d+)',
+        f'{onsite_z_field=:.3f}.nc']
+    )
+    unique_match = 0  # Check only one dataset is found.
+    for name in glob.glob(f'{data_dir}/*'):
+      match = re.search(regex, name)
+      if match is not None:
+        unique_match += 1
+        filename_match = name
+    if unique_match != 1:
+      raise ValueError(
+          f'Found {unique_match} files matching {regex} in {data_dir}'
+      )
+    return filename_match
+  return get_filename_fn
+
+
+def sweep_param_fn(
+      sampler,
+      system_size,
+      onsite_z_field,
+      train_d,
+      train_num_samples,
+      train_beta,
+      train_iter,
+  ) -> dict:
+  """Helper function for constructing sweep parameters.
+
+  Note: this function sweeps over dataset and training parameters.
+  Args:
+    sampler: dataset sampler name.
+    system_size: dataset system size.
+    onsite_z_field: dataset onsite z field.
+    train_d: bond dimension.
+    train_num_sample: number of training samples.
+    train_beta: regularization strength.
+    train_iterations: number of independent iterations to train over a model.
+  """
+  return {
+      'model.bond_dim': train_d,
+      'data.num_training_samples': train_num_samples,
+      'training.reg_kwargs.beta': train_beta,
+      'data.filename_fn': get_find_file_fn(
+          sampler, system_size, onsite_z_field
+      ),
+      'data.kwargs': {
+          'task_name': DEFAULT_TASK_NAME, 
+          'sampler': sampler, 'system_size': system_size, 
+          'onsite_z_field': onsite_z_field,
+      },
+      'results.filename': '_'.join(['%JOB_ID', DEFAULT_TASK_NAME,
+          sampler, f'{system_size=}', f'{onsite_z_field=:.3f}',
+          f'{train_d=}', f'{train_num_samples=}', f'{train_beta=:.3f}',
+          f'{train_iter=}']
+      ),
+      'training.iterations': train_iter,
+  }
+
+
+def sweep_sc_3x3_fn():
+  # 5x5 sites surface code sweep
+  for system_size in [3]:
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [5, 10, 20]:
+          for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
+            for train_beta in [0., 1., 5., 10.]:
+              for train_iter in range(10):
+                yield sweep_param_fn(
+                    sampler, system_size, onsite_z_field,
+                    train_d, train_num_samples, train_beta, train_iter
+                )
+
+
+def sweep_sc_5x5_fn():
+  # 5x5 sites surface code sweep
+  for system_size in [5]:
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [10, 20, 40]:
+          for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
+            for train_beta in [0., 1., 5., 10.]:
+              for train_iter in range(10):
+                yield sweep_param_fn(
+                    sampler, system_size, onsite_z_field,
+                    train_d, train_num_samples, train_beta, train_iter
+                )
+
+
+def sweep_sc_7x7_fn():
+  # 7x7 sites surface code sweep
+  for system_size in [7]:
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [20, 40, 60]:
+          for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
+            for train_beta in [0., 1., 5., 10.]:
+              for train_iter in range(10):
+                yield sweep_param_fn(
+                    sampler, system_size, onsite_z_field,
+                    train_d, train_num_samples, train_beta, train_iter
+                )
+
+
+SWEEP_FN_REGISTRY = {
+    "sweep_sc_3x3_fn": list(sweep_sc_3x3_fn()),
+    "sweep_sc_5x5_fn": list(sweep_sc_5x5_fn()),
+    "sweep_sc_7x7_fn": list(sweep_sc_7x7_fn())
+}
+
 
 def get_config():
   config = config_dict.ConfigDict()
+  # job properties.
+  config.job_id = config_dict.placeholder(int)
+  config.task_id = config_dict.placeholder(int)
+  # model.  
   config.model = config_dict.ConfigDict()
   config.model.bond_dim = 5
   config.model.dtype = 'complex128'
@@ -14,25 +137,41 @@ def get_config():
   # data.
   config.data = config_dict.ConfigDict()
   config.data.num_training_samples = 1000
-  config.data.path = ''.join(
-      [f'{home}/tn_shadow_dir/Data/Tests/', '0730_data.nc']
+  # CLUSTER: needs to be changed
+  config.data.dir = f'{HOME}/tn_shadow_dir/Data/{DEFAULT_TASK_NAME}'
+  config.data.kwargs = {
+      'task_name': DEFAULT_TASK_NAME, 
+      'sampler': 'xz_basis_sampler', 'system_size': 3, 'd': 10, 
+      'onsite_z_field': 0.0
+  }
+  config.data.filename = '_'.join([
+      '66321312', config.data.kwargs['task_name'], config.data.kwargs['sampler'],
+      f'system_size=3', 'd=10', 'onsite_z_field=0.000.nc']
   )
+  # Note: format of the data filename.
+  # ['%JOB_ID', '%TASK_NAME', '%SAMPLER', '%SYSTEM_SIZE', '%D',
+  # '%ONSITE_Z_FIELD']
+  config.data.filename_fn = None # Not needed if we know filename.
+  # sweep parameters.
+  # CLUSTER: could change this in slurm script
+  config.sweep_name = None  
+  config.sweep_fn_registry = SWEEP_FN_REGISTRY
   # training.
   config.training = config_dict.ConfigDict()
-  config.training.num_training_steps = 10
+  # Note for benchmarking a model, we want to train independently for many
+  # iterations. This will give us a better estimate of the model performance.
+  # Need to use this to change initial random seed. This is achieved in 
+  # setting iterations in `sweep_param_fn`.
+  config.training.iterations = 0  # Number of independent iterations.
+  config.training.num_training_steps = 200
   config.training.opt_kwargs = {}
   config.training.reg_name = 'hamiltonian'
-  config.training.reg_kwargs = {'beta': 1., 'estimator': 'mps'}
-  # physical system.
-  config.task_name = 'surface_code'  #TODO(YT): consider loading from dataset. 
-  # use zero field surface code to get only stabilizer MPOs.
-  #TODO(YT): consider resetting onsite_z_field from dataset. 
-  config.task_kwargs = {'size_x': 3, 'size_y': 3, 'onsite_z_field': 0.}
+  config.training.reg_kwargs = {'beta': 0., 'estimator': 'mps'}
   # Save options.
-  config.output = config_dict.ConfigDict()
-  config.output.save_results = True
-  config.output.experiment_dir = f'{home}/tn_shadow_dir/Outputs/Tests/'
-  config.output.results_save_path = os.path.join(
-      [config.output.experiment_dir, '%date_data']  #TODO(YT): move this to main_file.
+  config.results = config_dict.ConfigDict()
+  config.results.save_results = True
+  config.results.experiment_dir = ''.join([
+      f'{HOME}/tn_shadow_dir/Results', '/Tests/%CURRENT_DATE/']
   )
+  config.results.filename = '%JOB_ID_%TASK_ID'
   return config
