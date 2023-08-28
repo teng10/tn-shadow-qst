15ccdd1a4aa4ff43b74968f70abcb0714d557024
teng10 <adateng07@gmail.com>
2023-08-28 13:44:54 -0400

    Modified sweep parameters for surface code train config.

--- tn_generative/data_configs/surface_code_data_config.py
+++ tn_generative/data_configs/surface_code_data_config.py
@@ -5,61 +5,138 @@ import numpy as np
 from ml_collections import config_dict
 
 home = os.path.expanduser('~')
+DEFAULT_TASK_NAME = 'surface_code'
 
 
-task_name = 'surface_code'
-sampling_method = 'xz_basis_sampler'
+def sweep_param_fn(size_x, size_y, d, onsite_z_field, sampler):
+  """Helper function for constructing sweep parameters."""
+  return {
+      'task.kwargs.size_x': size_x,
+      'task.kwargs.size_y': size_y,
+      'dmrg.bond_dims': d,
+      'task.kwargs.onsite_z_field': onsite_z_field,
+      'output.filename':  '_'.join(['%JOB_ID', DEFAULT_TASK_NAME, sampler,
+          f'{size_x=}', f'{size_y=}', f'{d=}', f'{onsite_z_field=:.3f}']),
+  }
+
 
 def sweep_sc_7x7_fn():
   """Helper function for sweeping over surface code configs."""
   # 7x7 sites surface code sweep
-  for system_size in [7]:
-    for d in [60, 80, 120, 160]:
-      for onsite_z_field in np.linspace(0., 0.5, 21):
-        yield {
-            'task.kwargs.size_x': system_size,
-            'task.kwargs.size_y': system_size,
-            'dmrg.bond_dims': d,
-            'task.kwargs.onsite_z_field': onsite_z_field,
-            'output.filename':  '_'.join(['%JOB_ID', task_name, sampling_method,
-                f'{system_size=}', f'{d=}', f'{onsite_z_field=:.3f}']),
-        }
+  size_x = 7
+  size_y = 7
+  for d in [40, 60]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
 
 
 def sweep_sc_5x5_fn():
   """Helper function for sweeping over surface code configs."""
   # 5x5 sites surface code sweep
-  for system_size in [5]:
-    for d in [40, 60, 80, 100]:
-      for onsite_z_field in np.linspace(0., 0.5, 21):
-        yield {
-            'task.kwargs.size_x': system_size,
-            'task.kwargs.size_y': system_size,
-            'dmrg.bond_dims': d,
-            'task.kwargs.onsite_z_field': onsite_z_field,
-            'output.filename': '_'.join(['%JOB_ID', task_name, sampling_method,
-                f'{system_size=}', f'{d=}', f'{onsite_z_field=:.3f}']),
-        }
+  size_x = 5
+  size_y = 5
+  for d in [20, 40]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
+
 
 def sweep_sc_3x3_fn():
   # 3x3 sites surface code sweep
-  for system_size in [3]:
-    for d in [10, 20]:
-      for onsite_z_field in np.linspace(0., 0.5, 21):
-        yield {
-            'task.kwargs.size_x': system_size,
-            'task.kwargs.size_y': system_size,
-            'dmrg.bond_dims': d,
-            'task.kwargs.onsite_z_field': onsite_z_field,
-            'output.filename': '_'.join(['%JOB_ID', task_name, sampling_method,
-                f'{system_size=}', f'{d=}', f'{onsite_z_field=:.3f}']),
-        }
+  size_x = 3
+  size_y = 3
+  for d in [5, 10]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
 
-sweep_fn_dict = {
+
+def sweep_sc_3x5_fn():
+  # 3x3 sites surface code sweep
+  size_x = 3
+  size_y = 5
+  for d in [10, 20]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
+
+
+def sweep_sc_3x7_fn():
+  # 3x3 sites surface code sweep
+  size_x = 3
+  size_y = 7
+  for d in [10, 20]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
+
+
+def sweep_sc_3x9_fn():
+  # 3x3 sites surface code sweep
+  size_x = 3
+  size_y = 9
+  for d in [20, 40]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
+
+
+def sweep_sc_3x11_fn():
+  # 3x3 sites surface code sweep
+  size_x = 3
+  size_y = 11
+  for d in [40, 60]:
+    for onsite_z_field in np.linspace(0., 0.1, 2):
+      for sampler in [
+          'xz_basis_sampler', 'x_or_z_basis_sampler',
+          'x_y_z_basis_sampler',
+      ]:
+        yield sweep_param_fn(size_x=size_x, size_y=size_y, d=d,
+            onsite_z_field=onsite_z_field, sampler=sampler
+        )
+
+
+SWEEP_FN_REGISTRY = {
     "sweep_sc_3x3_fn": list(sweep_sc_3x3_fn()),
     "sweep_sc_5x5_fn": list(sweep_sc_5x5_fn()),
-    "sweep_sc_7x7_fn": list(sweep_sc_7x7_fn())
+    "sweep_sc_7x7_fn": list(sweep_sc_7x7_fn()), 
+    "sweep_sc_3x5_fn": list(sweep_sc_3x5_fn()),
+    "sweep_sc_3x7_fn": list(sweep_sc_3x7_fn()),
+    "sweep_sc_3x9_fn": list(sweep_sc_3x9_fn()),
+    "sweep_sc_3x11_fn": list(sweep_sc_3x11_fn()),
 }
+
 
 def get_config():
   """config using surface code as an example."""
@@ -70,12 +147,11 @@ def get_config():
   # Task configuration.
   config.dtype = 'complex128'
   config.task = config_dict.ConfigDict()
-  config.task.name = task_name
+  config.task.name = DEFAULT_TASK_NAME
   config.task.kwargs = {'size_x': 5, 'size_y': 5, 'onsite_z_field': 0.1}
   # sweep parameters.
-  config.sweep_param = list(sweep_sc_5x5_fn())
   config.sweep_name = "sweep_sc_3x3_fn"  # Could change this in slurm script
-  config.sweep_fn_dict = sweep_fn_dict
+  config.sweep_fn_registry = SWEEP_FN_REGISTRY
   # DMRG configuration.
   config.dmrg = config_dict.ConfigDict()
   config.dmrg.bond_dims = 10
@@ -84,7 +160,7 @@ def get_config():
   }
   # Sampler configuration.
   config.sampling = config_dict.ConfigDict()
-  config.sampling.sampling_method = sampling_method
+  config.sampling.sampling_method = 'x_or_z_basis_sampler'
   config.sampling.init_seed = 42
   config.sampling.num_samples = 100_000
   # Save options.
