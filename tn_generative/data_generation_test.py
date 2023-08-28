f1aaef265646cd4af88bb3636c07bc103539d76a
teng10 <adateng07@gmail.com>
2023-08-28 13:20:39 -0400

    Modified tests for data generationa and training.

--- tn_generative/data_generation_test.py
+++ tn_generative/data_generation_test.py
@@ -2,7 +2,10 @@
 import tempfile
 from absl.testing import absltest
 
+from ml_collections import config_dict
+
 from tn_generative.data_configs import surface_code_data_config
+from tn_generative.data_configs import ruby_pxp_data_config
 from tn_generative  import data_generation
 from tn_generative  import run_data_generation
 from tn_generative  import types
@@ -15,20 +18,35 @@ class RunDataGeneration(absltest.TestCase):
   """Tests data generation."""
 
   def setUp(self):
-    """Set up config for data generation using surface code."""
-    self.config = surface_code_data_config.get_config()
-    self.config.job_id = 0
-    self.config.task_id = 0
-    self.config.task.kwargs = {'size_x': 3, 'size_y': 3, 'onsite_z_field': 0.1}
-    self.config.dmrg.bond_dims = 5
-    self.config.sampling.num_samples = 500
-    self.config.output.data_dir = tempfile.mkdtemp('temp')
+    """Set up config for data generation for testing."""
+    self.config_dict = {}
+    self.config_dict['job_id'] = 0
+    self.config_dict['task_id'] = 0
+    self.config_dict['task.kwargs'] = {
+        'size_x': 3, 'size_y': 3, 'onsite_z_field': 0.1
+    }
+    self.config_dict['dmrg.bond_dims'] = 5
+    self.config_dict['sampling.num_samples'] = 500
+    self.config_dict['output.data_dir'] = tempfile.mkdtemp('temp')
 
   def test_generate_surface_code(self):
     """Tests data generation for surface code."""
-    config = self.config
+    config = surface_code_data_config.get_config()
+    config.update_from_flattened_dict(self.config_dict)
     run_data_generation.generate_data(config)
 
+  def test_generate_ruby_pxp(self):
+    """Tests data generation for rydberg hamiltonian PXP model."""
+    config = ruby_pxp_data_config.get_config()
+    config.update_from_flattened_dict(self.config_dict)
+    run_data_generation.generate_data(config)
+
+  #TODO(YT): finish cluster state PR
+  # def test_generate_cluster_state(self):
+  #   """Tests data generation for cluster state."""
+  #   config = cluster_state_data_config.get_config()
+  #   config.update_from_flattened_dict(self.config_dict)
+  #   run_data_generation.generate_data(config)
 
   if __name__ == '__main__':
     absltest.main()
--- tn_generative/train_utils_test.py
+++ tn_generative/train_utils_test.py
@@ -18,14 +18,13 @@ class RunTrainingTests(absltest.TestCase):
     # Get the path of the currently executing script
     script_path = os.path.abspath(__file__)
     # Extract the directory containing the script
-    current_file_dir = os.path.dirname(script_path)    
+    current_dir = os.path.dirname(script_path)    
     self.experiment_config = surface_code_training_config.get_config()
-    self.experiment_config.output.save_data = False
+    self.experiment_config.results.save_results = False
     self.experiment_config.job_id = 1
     self.experiment_config.task_id = 0
-    self.experiment_config.data.path = os.path.join(
-        current_file_dir, 'test_data', 'example_dataset.nc'
-    )
+    self.experiment_config.data.dir = os.path.join(current_dir, 'test_data')
+    self.experiment_config.data.filename = 'example_dataset.nc'
     self.experiment_config.data.num_training_samples = 1000
     self.experiment_config.training.num_training_steps = 10
     self.experiment_config.model.bond_dim = 5
