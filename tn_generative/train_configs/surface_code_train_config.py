344b9c9124af56e782be7766ade26813376f8831
teng10 <adateng07@gmail.com>
2023-09-24 22:22:11 -0400

    Modified train config for generic system sizes.

--- tn_generative/train_configs/surface_code_train_config.py
+++ tn_generative/train_configs/surface_code_train_config.py
@@ -10,23 +10,38 @@ DEFAULT_TASK_NAME = 'surface_code'
 
 def get_dataset_name(
     sampler,
-    system_size,
+    size_x,
+    size_y,
     onsite_z_field
  ) -> str:
   """Select the right dataset filename from available filenames."""
   # COMMENT: these lines are currently too long, but I don't know how to break.
   filenames = [
-      '66321312_surface_code_xz_basis_sampler_system_size=3_d=10_onsite_z_field=0.000.nc',
-      '66321941_surface_code_xz_basis_sampler_system_size=5_d=40_onsite_z_field=0.000.nc',
-      '66322999_surface_code_xz_basis_sampler_system_size=7_d=60_onsite_z_field=0.000.nc',
-      '66324730_surface_code_x_or_z_basis_sampler_system_size=3_d=10_onsite_z_field=0.000.nc',
-      '66325458_surface_code_x_or_z_basis_sampler_system_size=5_d=40_onsite_z_field=0.000.nc',
-      '66325485_surface_code_x_or_z_basis_sampler_system_size=7_d=60_onsite_z_field=0.000.nc',
+      '66321312_surface_code_xz_basis_sampler_size_x=3_size_y=3_d=10_onsite_z_field=0.000.nc',
+      '66321941_surface_code_xz_basis_sampler_size_x=5_size_y=5_d=40_onsite_z_field=0.000.nc',
+      '66322999_surface_code_xz_basis_sampler_size_x=7_size_y=7_d=60_onsite_z_field=0.000.nc',
+      '66324730_surface_code_x_or_z_basis_sampler_size_x=3_size_y=3_d=10_onsite_z_field=0.000.nc',
+      '66325458_surface_code_x_or_z_basis_sampler_size_x=5_size_y=5_d=40_onsite_z_field=0.000.nc',
+      '66325485_surface_code_x_or_z_basis_sampler_size_x=7_size_y=7_d=60_onsite_z_field=0.000.nc',
+      '2771128_surface_code_x_or_z_basis_sampler_size_x=3_size_y=9_d=40_onsite_z_field=0.000.nc',
+      '2771117_surface_code_xz_basis_sampler_size_x=3_size_y=7_d=20_onsite_z_field=0.000.nc',
+      # '2867669_surface_code_x_or_z_basis_sampler_size_x=3_size_y=3_d=10_onsite_z_field=0.000.nc',
+      # '2867669_surface_code_xz_basis_sampler_size_x=3_size_y=3_d=10_onsite_z_field=0.000.nc',
+      '2771128_surface_code_xz_basis_sampler_size_x=3_size_y=9_d=40_onsite_z_field=0.000.nc',
+      '2771117_surface_code_x_y_z_basis_sampler_size_x=3_size_y=7_d=20_onsite_z_field=0.000.nc',
+      '2868588_surface_code_x_y_z_basis_sampler_size_x=3_size_y=11_d=80_onsite_z_field=0.000.nc',
+      '2868588_surface_code_x_or_z_basis_sampler_size_x=3_size_y=11_d=80_onsite_z_field=0.000.nc',
+      '2771117_surface_code_x_or_z_basis_sampler_size_x=3_size_y=7_d=20_onsite_z_field=0.000.nc',
+      '2868588_surface_code_xz_basis_sampler_size_x=3_size_y=11_d=80_onsite_z_field=0.000.nc',
+      '2867669_surface_code_x_y_z_basis_sampler_size_x=3_size_y=3_d=10_onsite_z_field=0.000.nc',
+      '2771105_surface_code_x_y_z_basis_sampler_size_x=3_size_y=5_d=10_onsite_z_field=0.000.nc',
+      '2771105_surface_code_x_or_z_basis_sampler_size_x=3_size_y=5_d=10_onsite_z_field=0.000.nc',
+      '2771105_surface_code_xz_basis_sampler_size_x=3_size_y=5_d=10_onsite_z_field=0.000.nc',      
   ]
   unique_match = 0  # Check only one dataset is found.
   for name in filenames:
     regex = '_'.join([
-        DEFAULT_TASK_NAME, sampler, f'{system_size=}', 'd=(\d+)',
+        DEFAULT_TASK_NAME, sampler, f'{size_x=}', f'{size_y=}', 'd=(\d+)',
         f'{onsite_z_field=:.3f}.nc']
     )
     if re.search(regex, name) is not None:
@@ -41,7 +56,8 @@ def get_dataset_name(
 
 def sweep_param_fn(
     sampler,
-    system_size,
+    size_x,
+    size_y,
     onsite_z_field,
     train_d,
     train_num_samples,
@@ -54,7 +70,8 @@ def sweep_param_fn(
 
   Args:
     sampler: dataset sampler name.
-    system_size: dataset system size.
+    size_x: dataset system size x.
+    size_y: dataset system size y.
     onsite_z_field: dataset onsite z field.
     train_d: bond dimension.
     train_num_sample: number of training samples.
@@ -68,14 +85,9 @@ def sweep_param_fn(
       'model.bond_dim': train_d,
       'data.num_training_samples': train_num_samples,
       'training.reg_kwargs.beta': train_beta,
-      'data.filename': get_dataset_name(sampler, system_size, onsite_z_field),
-      'data.kwargs': {
-          'task_name': DEFAULT_TASK_NAME,
-          'sampler': sampler, 'system_size': system_size,
-          'onsite_z_field': onsite_z_field,
-      },
+      'data.filename': get_dataset_name(sampler, size_x, size_y, onsite_z_field),
       'results.filename': '_'.join(['%JOB_ID', DEFAULT_TASK_NAME,
-          sampler, f'{system_size=}', f'{onsite_z_field=:.3f}',
+          sampler, f'{size_x=}', f'{size_y=}', f'{onsite_z_field=:.3f}',
           f'{train_d=}', f'{train_num_samples=}', f'{train_beta=:.3f}',
           f'{init_seed=}']
       ),
@@ -84,60 +96,132 @@ def sweep_param_fn(
 
 
 def sweep_sc_3x3_fn():
-  # 5x5 sites surface code sweep
-  for system_size in [3]:
-    for init_seed in range(10):
-      for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
-        for onsite_z_field in [0.]:
-          for train_d in [5, 10, 20]:
-            for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
-              for train_beta in [0., 1., 5., 10.]:
-                yield sweep_param_fn(
-                    sampler=sampler, system_size=system_size,
-                    onsite_z_field=onsite_z_field, train_d=train_d,
-                    train_num_samples=train_num_samples, train_beta=train_beta,
-                    init_seed=init_seed,
-                )
+  size_x = 3
+  size_y = 3
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler', 'x_y_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [5, 10]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1., 5.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
 
 
 def sweep_sc_5x5_fn():
-  # 5x5 sites surface code sweep
-  for system_size in [5]:
-    for init_seed in range(10):
-      for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
-        for onsite_z_field in [0.]:
-          for train_d in [10, 20, 40]:
-            for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
-              for train_beta in [0., 1.]:
-                yield sweep_param_fn(
-                    sampler=sampler, system_size=system_size,
-                    onsite_z_field=onsite_z_field, train_d=train_d,
-                    train_num_samples=train_num_samples, train_beta=train_beta,
-                    init_seed=init_seed,
-                )
+  size_x = 5
+  size_y = 5
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [10, 20, 40]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
 
 
 def sweep_sc_7x7_fn():
-  # 7x7 sites surface code sweep
-  for system_size in [7]:
-    for init_seed in range(10):
-      for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
-        for onsite_z_field in [0.]:
-          for train_d in [20, 40, 60]:
-            for train_num_samples in [5000, 10_000, 30_000, 50_000, 100_000]:
-              for train_beta in [0., 1.]:
-                yield sweep_param_fn(
-                    sampler=sampler, system_size=system_size,
-                    onsite_z_field=onsite_z_field, train_d=train_d,
-                    train_num_samples=train_num_samples, train_beta=train_beta,
-                    init_seed=init_seed,
-                )
+  size_x = 7
+  size_y = 7
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [20, 40, 60]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
+
+
+def sweep_sc_3x5_fn():
+  size_x = 3
+  size_y = 5
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler', 'x_y_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [10, 20]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1., 5.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
+
+
+def sweep_sc_3x7_fn():
+  size_x = 3
+  size_y = 7
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler', 'x_y_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [20, 30]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1., 5.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
+
+
+def sweep_sc_3x9_fn():
+  size_x = 3
+  size_y = 9
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler', 'x_y_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [40, 50]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1., 5.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
+
+
+def sweep_sc_3x11_fn():
+  size_x = 3
+  size_y = 11
+  for init_seed in range(10):
+    for sampler in ['xz_basis_sampler', 'x_or_z_basis_sampler', 'x_y_z_basis_sampler']:
+      for onsite_z_field in [0.]:
+        for train_d in [80, 100]:
+          for train_num_samples in [100, 500, 3_000, 20_000, 100_000]:
+            for train_beta in [0., 1., 5.]:
+              yield sweep_param_fn(
+                  sampler=sampler, size_x=size_x, size_y=size_y,
+                  onsite_z_field=onsite_z_field, train_d=train_d,
+                  train_num_samples=train_num_samples, train_beta=train_beta,
+                  init_seed=init_seed,
+              )
 
 
 SWEEP_FN_REGISTRY = {
     'sweep_sc_3x3_fn': list(sweep_sc_3x3_fn()),
     'sweep_sc_5x5_fn': list(sweep_sc_5x5_fn()),
-    'sweep_sc_7x7_fn': list(sweep_sc_7x7_fn())
+    'sweep_sc_7x7_fn': list(sweep_sc_7x7_fn()),
+    'sweep_sc_3x5_fn': list(sweep_sc_3x5_fn()),
+    'sweep_sc_3x7_fn': list(sweep_sc_3x7_fn()),
+    'sweep_sc_3x9_fn': list(sweep_sc_3x9_fn()),
+    'sweep_sc_3x11_fn': list(sweep_sc_3x11_fn()),
 }
 
 
@@ -156,18 +240,18 @@ def get_config():
   config.data.num_training_samples = 1000
   # CLUSTER: needs to be changed
   config.data.dir = f'{HOME}/tn_shadow_dir/Data/{DEFAULT_TASK_NAME}'
-  config.data.kwargs = {
+  data_kwargs = {
       'task_name': DEFAULT_TASK_NAME,
-      'sampler': 'xz_basis_sampler', 'system_size': 3, 'd': 10,
+      'sampler': 'xz_basis_sampler', 'size_x': 3, 'size_y': 3, 'd': 10,
       'onsite_z_field': 0.0
   }
   config.data.filename = '_'.join([
-      '66321312', config.data.kwargs['task_name'], 
-      config.data.kwargs['sampler'], 'system_size=3',
+      '66321312', data_kwargs['task_name'], 
+      data_kwargs['sampler'], 'size_x=3', 'size_y=3',
       'd=10', 'onsite_z_field=0.000.nc']
   )
   # Note: format of the data filename.
-  # ['%JOB_ID', '%TASK_NAME', '%SAMPLER', '%SYSTEM_SIZE', '%D',
+  # ['%JOB_ID', '%TASK_NAME', '%SAMPLER', '%SIZE_X', '%SIZE_Y', '%D',
   # '%ONSITE_Z_FIELD']
   # sweep parameters.
   # CLUSTER: could change this in slurm script
