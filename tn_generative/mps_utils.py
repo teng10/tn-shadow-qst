bug: fix hadamard y rotation.

tn_generative/mps_utils.py
@@ -13,7 +13,7 @@ from tn_generative import types
 Array = types.Array
 
 HADAMARD = qmb.gen.operators.hadamard()
-Y_HADAMARD = 1./ np.sqrt(2.) * np.array([[0.0, -1.0j], [1.0j, 0.0]])
+Y_HADAMARD = 1./ np.sqrt(2.) * np.array([[-1.j, -1.], [1, 1.j]])
 EYE = qmb.gen.operators.eye(2)
 
 