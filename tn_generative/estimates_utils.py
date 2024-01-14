7df6c850423c62bf7e750f269dac39ec0d5735e6
teng10 <adateng07@gmail.com>
2024-01-14 10:48:06 +0100

    test: aded tests for estimate_expval_pauli_from_measurements function in estimates_utils. Added options for estimator in shadow_utils; added surface_code_xz dataset for testing.

--- tn_generative/estimates_utils.py
+++ tn_generative/estimates_utils.py
@@ -31,6 +31,8 @@ def _extract_non_identity_mpo(
   """
   def add_identity_tags(mpo, tag='I_like'):
     """Returns mpo with operators that are exactly identity tagged."""
+    mpo = mpo.copy() # Don't modify original mpo.
+    # Add tag to all identity tensors.
     [x.add_tag(tag) for x in mpo if np.allclose(x.data, qu.pauli('I'))]
     return mpo
   non_identity = add_identity_tags(mpo).select(
@@ -41,6 +43,34 @@ def _extract_non_identity_mpo(
     return non_identity, indices
   else:
     return non_identity
+
+
+def _extract_pauli_prod_from_ds(
+    ds: xr.Dataset,
+    pauli: np.ndarray,
+    indices = Sequence[int],
+) -> xr.Dataset:
+  """Compute pauli product for `pauli` of subsystem index `indices`.
+
+  Args:
+    ds: dataset containing `measurement`, `basis`.
+    pauli: pauli word to estimate expectation value.
+    indices: indices of subsystems of the pauli word.
+
+  Returns:
+    Pauli product of `pauli` for subsystems `indices`.
+  """
+  # Step 0: extract measurements from `ds` for `indices` and `pauli`.
+  ds_subsystem = ds.sel(site=indices)
+  ds_pauli = ds_subsystem.where(ds_subsystem.basis == pauli, drop=True)
+  # Step 1: compute the product of measurements for each sample.
+  def _pauli_prod(array, axis):
+    return np.prod(-2. * array + 1, axis=axis) # 0->1, 1->-1
+  pauli_prod = xr.apply_ufunc(
+      _pauli_prod, ds_pauli.measurement,
+      input_core_dims=[['site']], kwargs={'axis': -1}
+  )
+  return pauli_prod.dropna(dim='sample')
 
 
 def estimate_expval_pauli_from_measurements(
@@ -54,40 +84,30 @@ def estimate_expval_pauli_from_measurements(
 
   Note: this function direclty computes the average of measurement outcomes.
   It should be used when the number of measurements in pauli is large enough.
-  `estimator` can be one of: `empirical`, `median of means`, etc.
+  `estimator` can be one of: `empirical`, `mom`(median of means), etc.
   Errorbar is standard deviation / sqrt(number of samples).
-  
+
   Args:
     ds: dataset containing `measurement`, `basis`.
     pauli: pauli word to estimate expectation value.
     indices: indices of subsystems of the pauli word.
     estimator: method for estimating expectation value.
     return_err: whether to return the errorbar of the estimator.
-  
+
   Returns:
     Expectation value of `mpo` by selecting correct measurements from `ds`.
   """
-  # Step 0: extract measurements from `ds` for `indices` and `pauli`.
-  ds_subsystem = ds.sel(site=indices)
-  ds_pauli = ds_subsystem.where(ds_subsystem.basis == pauli, drop=True)
-  # Step 1: compute the product of measurements for each sample.
-  def _pauli_prod(array, axis):
-    return np.prod(-2. * array + 1, axis=axis) # 0->1, 1->-1
-  pauli_prod = xr.apply_ufunc(
-      _pauli_prod, ds_pauli.measurement,
-      input_core_dims=[['site']], kwargs={'axis': -1}
-  )
-  pauli_prod = pauli_prod.dropna(dim='sample')
-  # Step 2: compute the average of the product of measurements.
+  pauli_prod = _extract_pauli_prod_from_ds(ds, pauli, indices)
+  # compute the average of the product of measurements.
   if estimator == 'empirical':
     mean = pauli_prod.mean(dim='sample').values
     std = pauli_prod.std(dim='sample').values
-  elif estimator == 'median of means':
+  elif estimator == 'mom':
     raise NotImplementedError(f'Pauli {estimator=} not implemented.')
   else:
     raise ValueError(f'Unknown pauli {estimator=}.')
   if return_err:
-    return mean, std / np.sqrt(pauli_prod.shape[0])
+    return mean, std / np.sqrt(pauli_prod.sizes['sample'])
   else:
     return mean
 
@@ -96,6 +116,7 @@ def estimate_expval_mpo_from_dataset(
     ds: xr.Dataset,
     mpo: qtn.MatrixProductOperator,
     method: str,
+    estimator: str = 'empirical',
 ) -> float:
   """Estimates expectation values of physical quantities from `ds`.
   Note: the `shadow` is in geneal not a true reduced density matrix but used for
@@ -105,6 +126,7 @@ def estimate_expval_mpo_from_dataset(
     ds: dataset containing `measurement`, `basis`.
     mpo: MPO to estimate expectation value.
     method: method (`shadow` or `measurement`) to estimate expectation value.
+    estimator: method for estimating expectation value. `empirical` or `mom`.
 
   Returns:
     Expectation value of `mpo` estimated from `ds`.
@@ -116,7 +138,7 @@ def estimate_expval_mpo_from_dataset(
     shadow_single_shot_fn = shadow_utils._get_shadow_single_shot_fn(ds)
     estimate_shadow_fn = functools.partial(
         shadow_utils.construct_subsystem_shadows,
-        shadow_single_shot_fn=shadow_single_shot_fn
+        shadow_single_shot_fn=shadow_single_shot_fn, estimator=estimator,
     )
     subsystem_shadow = estimate_shadow_fn(ds, sub_indices)
     # STEP 2: estimate expectation value of `mpo` from `subsystem_shadow`.
@@ -128,7 +150,7 @@ def estimate_expval_mpo_from_dataset(
     pauli = np.array([PAULIMAP[pauli] for pauli in pauli_string])
     # STEP 2: estimate expectation value of `pauli` from `ds`.
     return estimate_expval_pauli_from_measurements(
-        ds, pauli, sub_indices, estimator='empirical'
+        ds, pauli, sub_indices, estimator=estimator,
     )
   else:
     raise NotImplementedError(f'Estimation method {method} not implemented.')
@@ -139,30 +161,36 @@ def estimate_observable(
   mpo: qtn.MatrixProductOperator,
   method: str = 'mps',
   tolerance: float = 1e-6,
+  estimator: str = 'empirical',
 ) -> float:
   """Estimates expectation value of `mpo` with `train_ds` using `method`.
+
+  # TODO(YT): consider adding another wrapper function to select `method`
+  # automatically based on `basis` in `train_ds`.
+  # e.g. if `basis` is fixed XZ, then use `measurement` method.
 
   Args:
     train_ds: dataset from which to estimate expectation value.
     mpo: MPO to estimate expectation value.
     method: method to use for estimation. Should we either `mps`, `shadow` or
-    `placeholder`. Default is exact computation using 'mps'.
+    `measurement`. Default is exact computation using 'mps'.
+    tolerance: tolerance for checking whether expectation value is real.
+    estimator: method for estimating expectation value. Options are
+        `empirical` (default) or `mom` (median of means).
 
   Return:
     estimated expectation value.
   """
   def is_approximately_real(number):
     return abs(number.imag) < tolerance
-  if method == 'measurement':
-    return 1.
+  if method in ('measurement', 'shadow'):
+    return estimate_expval_mpo_from_dataset(train_ds, mpo, method, estimator)
   elif method == 'mps':
     mps = mps_utils.xarray_to_mps(train_ds)
     expectation_val = (mps.H @ (mpo.apply(mps)))
     if not is_approximately_real(expectation_val):
       raise ValueError(f'{expectation_val=} is not real.')
     return expectation_val.real
-  elif method == 'shadow':
-    return estimate_expval_mpo_from_dataset(train_ds, mpo, method)
   else:
     raise ValueError(f'Unexpected estimation method {method}.')
 
--- tn_generative/estimates_utils_test.py
+++ tn_generative/estimates_utils_test.py
@@ -1,12 +1,15 @@
 """Tests for estimates_utils.py."""
 from absl.testing import absltest
 from absl.testing import parameterized
+import os
 
 import numpy as np
 import quimb as qu
 import quimb.tensor as qtn
 import quimb.gen as qugen
+import xarray as xr
 
+from tn_generative import data_utils
 from tn_generative import estimates_utils
 from tn_generative import physical_systems
 
@@ -57,6 +60,110 @@ class ExtractNonIdentityMPO(parameterized.TestCase):
     )
     expected_op = np.kron(qu.pauli('x'), qu.pauli('y'))
     np.testing.assert_allclose(expected_op, subsystem_mpo.to_dense())
+
+
+class EstimateExpvalPauliFromMeasurements(parameterized.TestCase):
+  """Tests for estimating expectation values of pauli words."""
+
+  def setUp(self):
+    self.num_samples = 1000
+    # Load bell state dataaset.
+    script_path = os.path.abspath(__file__)
+    # Extract the directory containing the script
+    current_file_dir = os.path.dirname(script_path)
+    ds_path = os.path.join(current_file_dir, 'test_data/bell_state_ds.nc')
+    self.bell_state_ds = xr.load_dataset(ds_path)
+    # Load surface code dataset.
+    ds_path = os.path.join(current_file_dir, 'test_data/surface_code_xz.nc')
+    surface_code_ds = xr.load_dataset(ds_path)
+    surface_code_ds = surface_code_ds.isel(sample=slice(0, self.num_samples))
+    self.surface_code_ds = data_utils.combine_complex_ds(surface_code_ds)
+
+  def test_product_state(self):
+    """Test expectation values of pauli words on product state."""
+    with self.subTest('neel'):
+      # Neel state. Expectation value of Z is -1.
+      pauli = np.array([2, 2, 2])
+      sub_indices = [0, 1, 2]
+      measurements_z = np.tile(np.array([0, 1, 0]), (self.num_samples, 1))
+      ds = xr.Dataset({
+          'measurement': (['sample', 'site'], measurements_z),
+          'basis': (['sample', 'site'], 2. * np.ones((self.num_samples, 3))),
+      })
+      expected_ev = -1.
+      actual_ev = estimates_utils.estimate_expval_pauli_from_measurements(
+          ds, pauli, sub_indices, estimator='empirical'
+      )
+      np.testing.assert_allclose(expected_ev, actual_ev)
+
+    with self.subTest('000'):
+      # Computational product state with expectation value of Z is 1.
+      pauli = np.array([2, 2, 2])
+      sub_indices = [0, 1, 2]
+      measurements_z = np.tile(np.array([0, 0, 0]), (self.num_samples, 1))
+      ds = xr.Dataset({
+          'measurement': (['sample', 'site'], measurements_z),
+          'basis': (['sample', 'site'], 2. * np.ones((self.num_samples, 3))),
+      })
+      expected_ev = 1.
+      actual_ev = estimates_utils.estimate_expval_pauli_from_measurements(
+          ds, pauli, sub_indices, estimator='empirical'
+      )
+
+  def test_bell_state(self):
+    """Test expectation values of pauli words on Bell state."""
+    # Test expectation value of stabilizers on bell state of size 2.
+    if self.bell_state_ds.sizes['site'] != 2:
+      raise ValueError(f"Bell state dataset is \
+          {self.bell_state_ds.sizes['site']} not of size 2."
+      )
+    with self.subTest('pauli z'):
+      # Expectation value of Z is 1.
+      pauli = np.array([2, 2])
+      sub_indices = [0, 1]
+      expected_ev = 1.
+      actual_ev = estimates_utils.estimate_expval_pauli_from_measurements(
+          self.bell_state_ds, pauli, sub_indices, estimator='empirical'
+      )
+      np.testing.assert_allclose(expected_ev, actual_ev)
+
+    with self.subTest('pauli x'):
+      # Expectation value of X is 1.
+      pauli = np.array([0, 0])
+      sub_indices = [0, 1]
+      expected_ev = 1.
+      actual_ev = estimates_utils.estimate_expval_pauli_from_measurements(
+          self.bell_state_ds, pauli, sub_indices, estimator='empirical'
+      )
+      np.testing.assert_allclose(expected_ev, actual_ev)
+
+
+  def test_surface_code_stabilizers(self):
+    """Test the stabilizer expectations of surface code."""
+
+    with self.subTest('stabilizer z'):
+      z_stabilizers = [[0, 1], [1, 2, 4, 5], [3, 4, 6, 7], [7, 8]]
+      expected_ev = np.ones((len(z_stabilizers)))
+      actual_z_evs = []
+      for stabilizer in z_stabilizers:
+        pauli = 2. * np.ones_like(stabilizer)
+        ev = estimates_utils.estimate_expval_pauli_from_measurements(
+            self.surface_code_ds, pauli, stabilizer, estimator='empirical'
+        )
+        actual_z_evs.append(ev)
+      np.testing.assert_allclose(expected_ev, np.asarray(actual_z_evs))
+
+    with self.subTest('stabilizer x'):
+      x_stabilizers = [[2, 5], [0, 1, 3, 4], [4, 5, 7, 8], [3, 6]]
+      expected_ev = np.array([0.9959432,  0.95537525, 0.97565923, 0.9959432])
+      actual_x_evs = []
+      for stabilizer in x_stabilizers:
+        pauli = np.zeros_like(stabilizer)
+        ev = estimates_utils.estimate_expval_pauli_from_measurements(
+            self.surface_code_ds, pauli, stabilizer, estimator='empirical'
+        )
+        actual_x_evs.append(ev)
+      np.testing.assert_allclose(expected_ev, np.asarray(actual_x_evs))
 
 
 if __name__ == '__main__':
