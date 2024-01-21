485d6163dd70250a62fb95e015b974cc960f3e53
teng10 <adateng07@gmail.com>
2024-01-21 22:40:29 +0100

    cartesian product for test case.

--- tn_generative/regularizers_test.py
+++ tn_generative/regularizers_test.py
@@ -13,7 +13,6 @@ from tn_generative import physical_systems
 from tn_generative import regularizers
 
 
-# TODO(YT): [High Priority] Clean up tests for `hamiltonian` regularizer.
 class HamiltonianRegularizerTest(parameterized.TestCase):
   """Tests for regularization using reduced_density_matrices."""
   def setUp(self):
@@ -27,37 +26,26 @@ class HamiltonianRegularizerTest(parameterized.TestCase):
         'hamiltonian'
     ]
 
-  @parameterized.named_parameters(
-      {
-          'testcase_name': 'surface_code_hamiltonian_mps',
+  @parameterized.product(
+      ({
           'physical_system': physical_systems.SurfaceCode(3, 3)
-      },
+      }, ), 
+      ({'method': 'mps', 'atol': 1e-6}, {'method': 'shadow', 'atol': 1e-3})
   )
-  def test_hamiltonian_regularization_explicit(self, physical_system):
+  def test_hamiltonian_regularization_explicit(
+      self, physical_system, method, atol
+  ):
     """Tests that regularization is positive and zero for true state."""
-    with self.subTest('method=mps'):
-      reg_fn = self.get_reg_fn(physical_system, self.test_ds, method='mps')
+    with self.subTest(f'{method=}'):
+      reg_fn = self.get_reg_fn(physical_system, self.test_ds, method=method)
       with self.subTest('assert_positive'):
         mps_rand = qtn.MPS_rand_state(physical_system.n_sites, 2, seed=42)
         reg_val = reg_fn(mps_rand.arrays)
-        # Consider remove this test as this depends on the random seed.
         self.assertGreater(reg_val, 0.)
 
       with self.subTest('assert_zero'):
         mps = mps_utils.xarray_to_mps(self.test_ds).arrays
-        np.testing.assert_allclose(reg_fn(mps), 0., atol=1e-6)
-
-    with self.subTest('method=shadow'):
-      reg_fn = self.get_reg_fn(physical_system, self.test_ds, method='shadow')
-      with self.subTest('assert_positive'):
-        mps_rand = qtn.MPS_rand_state(physical_system.n_sites, 2, seed=42)
-        reg_val = reg_fn(mps_rand.arrays)
-        # Consider remove this test as this depends on the random seed.
-        self.assertGreater(reg_val, 0.)
-
-      with self.subTest('assert_zero'):
-        mps = mps_utils.xarray_to_mps(self.test_ds).arrays
-        np.testing.assert_allclose(reg_fn(mps), 0., atol=1e-3)
+        np.testing.assert_allclose(reg_fn(mps), 0., atol=atol)    
 
 
 class SubsystemsRegularizerTest(parameterized.TestCase):
